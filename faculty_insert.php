d20def398f052a9ecf1aeafc877a03a77c021a99
Justine Danielle L. Betito <justinebetito17@gmail.com>
2026-05-10 15:49:06 +0900

    Cleaned up repo to only include project files

--- faculty_insert.php
+++
@@ -1,9 +0,0 @@
-<form action="faculty_insert_save.php" method="post">
-    <h3>Add Faculty Member</h3>
-    ID Number: <input type="text" name="txtidno" required><br>
-    Last Name: <input type="text" name="txtlast" required><br>
-    First Name: <input type="text" name="txtfirst" required><br>
-    Middle Name: <input type="text" name="txtmiddle"><br>
-    <input type="submit" value="Save Record">
-    <button type="button" onclick="window.location.href='faculty.php'">Back</button>
-</form>
